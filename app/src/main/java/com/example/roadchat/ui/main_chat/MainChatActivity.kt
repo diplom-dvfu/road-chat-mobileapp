0145c78319e1d24e7b56df1684ca62316e4880f3
Ilia Khachatrian <31718172+quiksilver4210@users.noreply.github.com>
2021-06-01 16:40:52 +1000

    add login activity and view

---
+++ app/src/main/java/com/example/roadchat/ui/main_chat/MainChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.roadchat.ui.main_chat
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.roadchat.R
+
+class MainChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_chat)
+    }
+}
